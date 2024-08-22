040cdd78d025dcdfcd6c87fb7c59203d8b798302
pedrohgarbim <phg.garbim@gmail.com>
2024-08-22 11:54:09 -0300

    Substitui chai pelo sistema de asserção do Jest

--- tests/script.test.js
+++ tests/script.test.js
@@ -1,10 +1,9 @@
 const { JSDOM } = require('jsdom');
-const { expect } = require('chai');
 
 describe('Counter functionality', function() {
     it('should start at zero', function() {
         const dom = new JSDOM(`<!DOCTYPE html><div id="counter">0</div>`);
         const counterDisplay = dom.window.document.getElementById('counter');
-        expect(counterDisplay.textContent).to.equal('0');
+        expect(counterDisplay.textContent).toBe('0');
     });
 });
